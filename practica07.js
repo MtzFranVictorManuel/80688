d00b33f3ea4bc24b32005bd4c0e3ec2dad9aa283
MtzFranVictorManuel <zs22024137@estudiantes.uv.mx>
2023-09-21 14:48:35 -0600

    practica07

---
+++ practica07.js
@@ -0,0 +1,10 @@
+const formulario = document.getElementById("formulario");
+        const contenedor = document.getElementById("contenedor");
+        formulario.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const nombre = document.getElementById("nombre").value;
+            const mail = document.getElementById("mail").value;
+            const direc = document.getElementById("direc").value;
+            const ciudad = document.getElementById("ciudad").value;
+            
+        });
