bbf73ae63b7654305f15e6da221b45a3735a33f2
MtzFranVictorManuel <zs22024137@estudiantes.uv.mx>
2023-11-16 14:20:14 -0600

    practica10

--- practica09/practica09/src/main/java/mx/uv/App.java
+++ practica09/practica09/src/main/java/mx/uv/App.java
@@ -1,6 +1,10 @@
 package mx.uv;
 
 import static spark.Spark.*;
+
+import com.google.gson.*;
+
+
 /**
  * Hello world!
  *
@@ -9,12 +13,54 @@ public class App
 {
     public static void main( String[] args )
     {
-        get("/",  (request, response) -> "<h1>Hola mundo desde Spark</h1>");
+        System.out.println( "Hello World!" );
 
-        get("/hola",  (request, response) -> "<h1>Hola hola hola hola hola</h1>");
+        //fuente:https://gist.github.com/saeidzebardast/e375b7d17be3e0f4dddf
+        options("/*",(request,response)->{
+        String accessControlRequestHeaders=request.headers("Access-Control-Request-Headers");
+        if(accessControlRequestHeaders!=null){
+        response.header("Access-Control-Allow-Headers",accessControlRequestHeaders);
+        }
+        String accessControlRequestMethod=request.headers("Access-Control-Request-Method");
+        if(accessControlRequestMethod!=null){
+        response.header("Access-Control-Allow-Methods",accessControlRequestMethod);
+        }
+        return "OK";
+        });
+        before((request,response)->response.header("Access-Control-Allow-Origin","*"));
 
-        get("/adios",  (request, response) -> "<h1>Adios</h1>");
+        
+        get("/", 
+                (request, response) -> "<h1>Bienvenidx</h1>"
+        );
+        get("/hola", 
+                (request, response) -> "<h1>Hola mundo!</h1>"
+        );
+        get("/adios", 
+                (request, response) -> "<h1>Adios mundo!</h1>"
+        );
+        get("/alumno", 
+                (request, response) -> "{'alumno' : 'john','matricula' : 's0001','carrera' : 'tc'}"
+        );
 
-        get("/fin",  (request, response) -> "<h1>Fin</h1>");
+        post("/alumno", 
+                (request, response) -> {
+                        System.out.println(request.contentLength());
+                        System.out.println(request.contentType());
+                        System.out.println(request.contextPath());
+
+                        return "profesor: "+request.queryParams("paterno");
+                }
+        );
+
+        get("/tipo-usuario", (request, response) ->{
+                JsonObject respuesta = new JsonObject();
+                respuesta.addProperty("tipousuario", "profesor");
+                respuesta.addProperty("nombre", request.queryParams("nombre"));
+                respuesta.addProperty("paterno", request.queryParams("paterno"));
+                respuesta.addProperty("materno", request.queryParams("materno"));
+
+                return respuesta;
+        });
     }
 }
